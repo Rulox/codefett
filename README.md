35e2081258cfb2b27ffd61ec884fd2800928d88c
Raul Marrero <marreror91@gmail.com>
2015-12-02 21:23:40 +0000

    Generate Frontend config
    * Create Gruntfile and tasks for the JS/CSS compilation
    * Create bower.json file for basic libs (bootstrap, jquery, angular)
    * Create tasks to copy js and css to /static after compilation
    * Add basic image logos of the app

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# codafett
+# Codafett
 Web application for automatic testing codes from students
+# Description
+# Plugins
+# License
+# Credits
+
+# Dependencies (WIP)
+If you want to contribute to this project, please keep in mind the following dependencies. There
+are different dependencies if you are going to work in the backend or in the frontend.
+##### Dependencies (BackEnd)
+* Python 2.7
+* Django 1.8
+* ... See `requirements`
+
+##### Dependencies (FrontEnd)
+* Ruby-dev
+* Sass
+* Compass
+* Nodejs (npm)
+
+Raúl Marrero Rodríguez
+Universidad de La Laguna - Tenerife, Spain
---
+++ codefett/assets/Gruntfile.js
@@ -0,0 +1,160 @@
+module.exports = function(grunt) {
+ 
+    grunt.initConfig({
+ 
+        pkg: grunt.file.readJSON('package.json'),
+ 
+        // Pre-commands
+        run: {
+            prepare: {
+                exec: 'rm -rf ../static/ && mkdir ../static' // We want to clean all statics when running Grunt
+            },
+            bower: {
+                exec: 'bower install'
+            }
+        },
+
+        /**
+         *  ----------- JAVASCRIPT -------------
+         * 
+         */
+
+        // Concatenate JS files into main.js
+        concat: {
+            options: {
+                separator: '\n'
+            },
+            dist: {
+                src: ['./js/**/*.js'],
+                dest: '../static/js/main.js'
+            }
+        },
+
+        // Uglify JS files
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                        '* Copyright (c) <%= grunt.template.today("yyyy") %> */',
+            },
+            dist: {
+                files: {
+                    '../static/js/main.min.js': '../static/js/main.js'
+                }
+            }
+        },
+
+        // Check JS Syntax!
+        jshint: {
+            files: ['gruntfile.js', '../static/js/**/*.js'],
+            options: {
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true
+                }
+            }
+                
+        },
+
+
+        /**
+         *  ----------- SASS -------------
+         * 
+         */
+        // Compile SASS into CSS
+        compass: {
+            dist: {
+                options: {
+                    sassDir: './sass/**',
+                    cssDir: '../static/css',
+                    environment: 'production',
+                    outputStyle: 'compressed'
+                }
+            },
+            dev: {
+                options: {
+                    sassDir: './sass',
+                    cssDir: '../static/css',
+                    environment: 'development',
+                }
+            }
+        },
+
+        /**
+         *  ----------- OTHER TASKS-------------
+         * 
+         */
+
+        // Copy only useful files into /static after bower install
+        bowercopy: {
+            // Javascript files
+            js: {
+                options: {
+                    destPrefix: '../static/js'
+                },
+                files: {
+                    'bootstrap.min.js': 'bootstrap/dist/js/bootstrap.min.js',
+                    'jquery.min.js': 'jquery/dist/jquery.min.js',
+                    'jquery.min.map': 'jquery/dist/jquery.min.map',
+                    'angular.min.js': 'angular/angular.min.js',
+                    'angular.min.map': 'angular/angular.min.js.map'
+                }
+            },
+            // CSS Files
+            css: {
+                options: {
+                    destPrefix: '../static/css'
+                },
+                files: {
+                    'bootstrap.min.css': 'bootstrap/dist/css/bootstrap.min.css',
+                    'bootstrap.min.css.map': 'bootstrap/dist/css/bootstrap.min.css.map',
+                }
+            },
+            // Fonts files
+            fonts: {
+                options: {
+                    destPrefix: '../static/fonts'
+                },
+                files: {
+                    'glyphicons-halflings-regular.ttf': 'bootstrap/dist/fonts/glyphicons-halflings-regular.ttf'
+                }
+            },
+            // Images
+            fonts: {
+                options: {
+                    destPrefix: '../static/images'
+                },
+                files: {
+                    '': '../images/*'
+                }
+            }
+        },
+
+        // Watcher
+        watch: {
+            scripts: {
+                files: [
+                    './sass/**/*.scss',
+                    './js/**/*.js'
+                ],
+                tasks: ['concat', 'uglify']
+            }
+        }
+ 
+    });
+ 
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-run');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-bowercopy');
+
+ 
+    // Default task.
+    grunt.registerTask('default', ['run:prepare', 'run:bower', 'compass:dev', 'concat', 'uglify', 'bowercopy']);
+ 
+};
