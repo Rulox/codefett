3ba3bb525752de1c3d22173ee551070513dd4624
Raul Marrero <marreror91@gmail.com>
2015-12-03 22:24:26 +0000

    Create basic user model and test

--- codefett/users/models.py
+++ codefett/users/models.py
@@ -1,5 +1,21 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class CFUser(models.Model):
+    """
+    Basic User model for Codefett. It has a OneToOne relationship with Django User in order to implement
+    registration and login. We only want to ask for some basic details. Email must be unique so we can
+    identify users.
+    """
+    user = models.OneToOneField(User)
+    full_name = models.CharField(max_length=90)
+    avatar = models.ImageField(blank=True, null=True)
+
+    @property
+    def name(self):
+        return self.full_name.split(' ')[0]
+
+    def __unicode__(self):
+        return u'{} ({})'.format(self.name, self.user.email)
--- codefett/users/tests.py
+++ codefett/users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import CFUser
 
-# Create your tests here.
+
+class CFUserTest(TestCase):
+    def setUp(self):
+        dj_user = User.objects.create_user('john@codefett.com', password='foo', email='john@codefett.com')
+        CFUser.objects.create(user=dj_user, full_name='John Doe')
+
+    def TestUnicode(self):
+        john = CFUser.objects.get(user__email='john@codefett.com')
+
+        self.assertTrue('John (john@codefett.com)', john.__unicode__())
