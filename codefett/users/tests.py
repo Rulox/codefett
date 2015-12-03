928bb237e96c953c0d10390a20d9db87b02210f3
Raul Marrero <marreror91@gmail.com>
2015-12-03 22:27:14 +0000

    Change settings and users tests
    * Change settings in order to add Users app
    * Fix a problem with test definition

--- codefett/users/tests.py
+++ codefett/users/tests.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 from .models import CFUser
 
 
-class CFUserTest(TestCase):
+class CFUserTestCase(TestCase):
     def setUp(self):
         dj_user = User.objects.create_user('john@codefett.com', password='foo', email='john@codefett.com')
         CFUser.objects.create(user=dj_user, full_name='John Doe')
 
-    def TestUnicode(self):
+    def test_cfuser_unicode(self):
         john = CFUser.objects.get(user__email='john@codefett.com')
 
         self.assertTrue('John (john@codefett.com)', john.__unicode__())
