485257d45f78a9958911329a34c7c93d63aafb47
Rulox <marreror91@gmail.com>
2017-11-30 21:46:42 +0000

    Add courses list and detail

--- codefett/courses/urls.py
+++ codefett/courses/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import CourseDetail
+from .views import CourseDetail, CourseList
 
 urlpatterns = [
+    url(r'^$', CourseList.as_view(), name='course_list'),
     url(r'^(?P<pk>\d+)$', CourseDetail.as_view(), name='course_detail'),
 ]
--- codefett/courses/views.py
+++ codefett/courses/views.py
@@ -1,7 +1,31 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Course
+from django.http import HttpResponseForbidden
+from django import forms
 
 
+class PreparationForm(forms.Form):
+    ExampleProject = forms.FileField(label='Example Project')
+
+
+class CourseList(LoginRequiredMixin, ListView):
+    model = Course
+
+
+# FIXME change for normal view cos we need a form
 class CourseDetail(LoginRequiredMixin, DetailView):
     model = Course
+
+    def get_context_data(self, **kwargs):
+        context = super(CourseDetail, self).get_context_data(**kwargs)
+        context['form'] = PreparationForm
+        context['example_url'] = ''
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.course_set.filter(enrollment__course=self.get_object()).exists():
+            #TODO Add if the user is owner
+            return super(CourseDetail, self).dispatch(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
